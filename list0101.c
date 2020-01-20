cfc25128dfc6bf08b09ffd58b814504d72797350
ryota <ryota@ryotas-MacBook-Pro.local>
2020-01-21 03:13:15 +0900

    Day1 progress
    Lreaning what I prettly much know about, but did learn a lot terms that I used without knowing.

---
+++ list0101.c
@@ -0,0 +1,17 @@
+/*
+	誠意数値15と37を加えた結果を表示
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("15から37を引いた値は%dです。\n", 15-37);
+	printf("Hello. \a\n"); 
+	/*\a is escape sequence拡張表記*/
+	
+	int n = 3.14;
+	printf("%d\n", n);
+	/*This gives you a warning. */
+	
+	return 0;
+}

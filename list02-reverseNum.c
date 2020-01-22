1737af7153807e66427aa6151cf3a149a08d18a4
RyotaBannai <ryotala0528@gmail.com>
2020-01-23 01:45:11 +0900

    Learning array in C.

--- list02-reverseNum.c
+++ list02-reverseNum.c
@@ -8,9 +8,16 @@ int main(void)
 	int no;
 	do{
 		printf("Plese input positive number: ");
-		scanf("%d\n" ,$no);
+		scanf("%d" ,&no);
 		if(no < 1){
-			printf("\aPlease do not input 0 nor negative number.\n")
+			printf("\aPlease do not input 0 nor negative number.\n");
 		}
-	}while(no < 1)
+	}while(no < 1);
+	
+	while(no > 0){
+		printf("%d", no%10);
+		no /= 10;
+	}
+	putchar('\n');
+		
 }
---
+++ list05-array.c
@@ -0,0 +1,21 @@
+/*
+ let's try out array in C
+*/
+#include<stdio.h>
+
+int main(void)
+{
+	/* 要素数５の配列aを1~5で、bを全て0で初期化 */
+	int a[5]={1,2,3,4,5}, b[5]={0}, i;
+	/*
+	  Cでは配列を代入演算子でコピーできない
+		繰り返し文で全要素代入する
+	*/
+	for (i=0; i<(sizeof a / sizeof a[0]); i++){
+		b[i] = a[4-i];
+	}
+	printf("a, b\n");
+	for (i=0; i<(sizeof b / sizeof b[0]); i++){
+		printf("%d, %d\n", a[i], b[i]);
+	}
+}

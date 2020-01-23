f3e985a365e052f7683c7abd6de161f9f43a4417
RyotaBannai <ryotala0528@gmail.com>
2020-01-23 19:55:44 +0900

    Use functions in C.

---
+++ list06-usefunciton.c
@@ -0,0 +1,21 @@
+/*
+ Use function 
+*/
+#include <stdio.h>
+
+int square(int n){
+	return n*n;
+}
+int pow4(int n){
+	return square(square(n));
+}
+int diff(int a, int b){
+	/*Return bigger number*/
+	return a>b ? a-b: b-a;
+}
+
+int main(void){
+	int n1=2, n2=3;
+	printf("二乗誤差は %d　です。\n", diff(square(n1), square(n2)));
+	printf("４乗誤差は %d　です。\n", diff(pow4(n1), pow4(n2)));
+}

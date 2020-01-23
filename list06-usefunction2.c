a93f944fcd7bffb859658ef8bdb93ec361ddda70
RyotaBannai <ryotala0528@gmail.com>
2020-01-23 20:20:17 +0900

    Set void as argument.

---
+++ list06-usefunction2.c
@@ -0,0 +1,29 @@
+/*
+	引数を受け取らない関数にはvoidを明示する
+*/
+#include <stdio.h>
+
+int scan_pint(void){
+	int temp;
+	do{
+		printf("非負の整数を入力してください。:");
+		scanf("%d",&temp);
+		if (temp <= 0) puts("\a正でない整数を入力しないでください。");
+	}while(temp <= 0);
+	
+	return temp;
+}
+int rev_int(int num){
+	int temp = 0;
+	do{
+		temp = temp*10 + num%10; //tempの桁を上げる＆一の桁に加える
+		num /=10; //桁を下げる
+	}while (num > 0);
+	
+	return temp;
+}
+
+int main(void){
+	int num = scan_pint();
+	printf("入力した数値を反転させました: %d\n", rev_int(num));
+}

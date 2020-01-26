ebffe6f6439b4b94d1ce1514c3cfe408e6faf534
RyotaBannai <ryotala0528@gmail.com>
2020-01-27 00:02:47 +0900

    Be aware of declaration of x with x itself.

--- list06-scopes.c
+++ list06-scopes.c
@@ -9,15 +9,15 @@ void print_x(void){
 	printf("in print_x function: %3d\n",x);
 }
 int main(void){
-	int i, x=x; // main block
+	int i, x=x; // main block // Be aware of this. the right side of x is not x from file scipe. the x is from left side x, however, this statement is not complete when it's called. 
 	print_x();
-	printf("%3d",x);
+	printf("main func scope: %3d\n",x);
 	
 	for (i=1;i<5;i++){
 		int x=1; // for block
 		printf("%3d",x+i);
 	}
-	
-	printf("\n%3d\n",x);
+	putchar('\n');
+	printf("main func scope: %3d\n",x);
 	return 0;
 }

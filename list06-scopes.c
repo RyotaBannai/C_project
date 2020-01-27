67fdb5d61c05b5df9b5d96ddc3b7d805c732cccc
RyotaBannai <ryotala0528@gmail.com>
2020-01-27 23:58:40 +0900

    Checkout the range of varaible type on mac

--- list06-scopes.c
+++ list06-scopes.c
@@ -9,12 +9,12 @@ void print_x(void){
 	printf("in print_x function: %3d\n",x);
 }
 int main(void){
-	int i, x=x; // main block // Be aware of this. the right side of x is not x from file scipe. the x is from left side x, however, this statement is not complete when it's called. 
+	int i, x=x; // main block // Be aware of this. the right side of x is not x from file scipe. the x is from left side x, however, this statement hasnt completed yet when it's called, thus a randam number is assigned.
 	print_x();
 	printf("main func scope: %3d\n",x);
 	
 	for (i=1;i<5;i++){
-		int x=1; // for block
+		int x=1; // for loop block
 		printf("%3d",x+i);
 	}
 	putchar('\n');

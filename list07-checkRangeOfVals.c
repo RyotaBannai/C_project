0ecea14602aebbea744299aaa324d5d5cd6c5c8c
RyotaBannai <ryotala0528@gmail.com>
2020-01-28 01:10:19 +0900

    further test of size of variables.

--- list07-checkRangeOfVals.c
+++ list07-checkRangeOfVals.c
@@ -6,9 +6,14 @@
 
 int main(void){
 	
-	printf("char: %d~%d\n",CHAR_MIN, CHAR_MAX);
-	printf("int: %d~%d\n",SHRT_MIN, SHRT_MAC);
-	printf("int: %d~%d\n",INT_MIN, INT_MAX); // the renge is the same of long on mac
+	printf("char: %d~%d\n",CHAR_MIN, CHAR_MAX); // char 1byte
+	printf("short: %d~%d\n",SHRT_MIN, SHRT_MAX); //short 2byte
+	printf("int: %d~%d\n",INT_MIN, INT_MAX); //int 4byte
+	printf("long: %ld~%ld\n",LONG_MIN, LONG_MAX); //long 8byte
+	// %ld
 	
+	printf("unsigned: %d~%lu\n",0, ULONG_MAX); //%lu
+	// error: use of undeclared identifier 'ULONG_MIN' >> negative range is undeclared for unsigned variables.
+	// >> unsigned: 0~18446744073709551615
 	return 0;
 }
--- list07-sizeOfChar.c
+++ list07-sizeOfChar.c
@@ -11,4 +11,17 @@ int main(void){
 	printf("short:%u\n", (unsigned)sizeof(short)); // >>2
 	printf("int:%u\n", (unsigned)sizeof(int)); // >>4
 	printf("long:%lu\n", sizeof(long)); // >>8
+	
+	//what about int array
+	int array[2]={0};
+	printf("the size of whole array is %d, because int(=4byte) * 2 (elements in array)\n", (unsigned)sizeof array);
+	double a;
+	printf("double: %d\n", (unsigned)sizeof a); // >> 8 same as long
+	
+	//evaluate statement 
+	printf("evaluate statement 1: %d\n", (unsigned)sizeof 1); // >>4
+	printf("evaluate statement unsigned -1(=%d): %d\n", (unsigned)-1, (unsigned)sizeof (unsigned)-1); // >>3?
+	//負があったほうが処理が楽だったり、負になる可能性のあるものをunsignedで定義するのはバグの元。unsignedは加算減算・除乗算しないものやビット演算するものだけに限定すべき。
+	
+	return 0;
 }

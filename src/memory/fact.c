ab5b36a0dc78247fd9cba3e7dad01459505a3a13
RyotaBannai <ryotala0528@gmail.com>
2021-01-31 00:17:56 +0900

    add: array notes 2

--- src/memory/fact.c
+++ src/memory/fact.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#define LEN_ARR(x) (sizeof(x) / sizeof((x)[0])) // macro
 
 int fact(int n)
 {
   if (n == 1)
   {
-    return;
+    return 1;
   }
   else
   {
@@ -12,7 +13,18 @@ int fact(int n)
   }
 }
 
-void main()
+int main()
 {
-  fact(5);
+  // fact(5);
+  int array[10], n, *p;
+  for (n = 0, p = array; n < 10; ++n)
+  {
+    *p++ = 0;
+  }
+
+  int size_array = LEN_ARR(array);
+  for (n = 0; n < size_array; ++n)
+  {
+    printf("%d ", array[n]);
+  }
 }

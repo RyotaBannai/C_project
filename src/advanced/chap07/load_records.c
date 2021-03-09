a76ec55ddc8b4782433e89bfbedca36ba821315c
RyotaBannai <ryotala0528@gmail.com>
2021-03-09 23:49:41 +0900

    add: read load record codes.

--- src/advanced/chap07/load_records.c
+++ src/advanced/chap07/load_records.c
@@ -15,14 +15,15 @@ int fgetword(FILE *fp, char *str)
   while ((ch = fgetc(fp)) != EOF && ch != '\t' && ch != '\n')
   {
     if (ch != ' ')
-      *str++ = ch;
+      *str++ = ch; // 追加してアドレスをひとつ先に進める
   }
-  *str = '\0';
+  *str = '\0'; // 連続でタブが入っている（欠損値）の場合は、while からすぐ break するため null 文字を入れる.
   return ch;
 }
 
 int getrec(FILE *fp, struct rec *dat)
 {
+  printf("called\n");
   if (fgetword(fp, dat->a) == EOF)
     return EOF;
   if (fgetword(fp, dat->b) == EOF)
@@ -41,7 +42,9 @@ int main(void)
 {
   FILE *fp;
   struct rec dat;
-  fp = fopen("data.txt", "r");
+
+  fp = fopen("data2.txt", "r");
+  //  while の中身が EOF になるときに break するため, データの最後の行は改行が必要
   while (getrec(fp, &dat) == 0)
   {
     printf("%s\t%s\t%s\t%s\t%s\n", dat.a, dat.b, dat.c, dat.d, dat.e);
